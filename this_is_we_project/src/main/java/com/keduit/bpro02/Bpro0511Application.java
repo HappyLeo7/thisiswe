8ab066d6089b1681881cdc61952bfb3086c8ecaf
keduit <keduit@505-09>
2023-01-26 10:08:18 +0900

    테스트 프로젝트

---
+++ this_is_we_project/src/main/java/com/keduit/bpro02/Bpro0511Application.java
@@ -0,0 +1,13 @@
+package com.keduit.bpro02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bpro0511Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bpro0511Application.class, args);
+	}
+
+}
---
+++ this_is_we_project/src/test/java/com/keduit/bpro02/Bpro0511ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keduit.bpro02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bpro0511ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
