1295db7b9f62caadcc3b1d236298cd8b64255645
user <soochan7@gmail.com>
2023-02-11 14:44:43 +0900

    mongoDB atlas connect

--- this_is_we_project/src/main/java/com/thisiswe/home/chat/mongodb/UserService.java
+++ this_is_we_project/src/main/java/com/thisiswe/home/chat/mongodb/UserService.java
@@ -11,6 +11,7 @@ public class UserService {
     private final MongoTemplate mongoTemplate;
 
     public void mongoInsert() {
+        System.out.println("됐으요잉~ㄴ");
         AlarmLog alarmLog = new AlarmLog(1L, "제목", "메세지");
         mongoTemplate.insert(alarmLog);
     }
