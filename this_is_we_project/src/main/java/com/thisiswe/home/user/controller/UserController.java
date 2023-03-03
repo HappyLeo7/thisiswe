b8841c9680fbdbdbea326774d70181ddc4d56627
oso1207-github <112902601+oso1207-github@users.noreply.github.com>
2023-03-04 07:45:04 +0900

    [왓슨] 카카오 로그인 api 경로 재설정

--- this_is_we_project/src/main/java/com/thisiswe/home/user/controller/UserController.java
+++ this_is_we_project/src/main/java/com/thisiswe/home/user/controller/UserController.java
@@ -36,34 +36,38 @@ public class UserController {
 
 	// 회원 로그인 페이지
 	@GetMapping("/login")
-	public String login(HttpServletRequest request, HttpServletResponse response) {
+	public String login(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+
 		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
 		Cookie[] cookies = request.getCookies();
-		if (cookies != null) {
+		if (cookies != null && session.	getAttribute("userDetails") != null) {
 			for (Cookie cookie : cookies) {
-				if (cookie.getName().equals("loggedIn") && cookie.getValue().equals("true")) {
-					return "redirect:/thisiswe";
+				System.out.println("세션이 만료되었는지 확인 : " + session.getAttribute("userDetails"));
+					if (cookie.getName().equals("loggedIn") && cookie.getValue().equals("true")) {
+						/* session. */
+						return "redirect:/thisiswe";
+					}
 				}
 			}
-		}
+
 		return "login/login";
 	}
 
-	
-	// post login 이동
-	@PostMapping("/login")
-	public String loginsucces(@AuthenticationPrincipal UserDetailsImpl userDetails, HttpSession session) {
-		session.setAttribute("userDetails", userDetails);
-	    return "redirect:/thisiswe";
-	}
-	
-	// 회원 로그아웃
-	@PostMapping("/logout")
-	public String logout(HttpSession session) {
-		session.removeAttribute("userDetails");
-		return "redirect:/thisiswe";
-	}
-	
+	// 기존 login버튼이 남겨져 있을 때 
+//	// post login 이동
+//	@PostMapping("/login")
+//	public String loginsucces(@AuthenticationPrincipal UserDetailsImpl userDetails, HttpSession session) {
+//		session.setAttribute("userDetails", userDetails);
+//		return "redirect:/thisiswe";
+//	}
+//
+//	// 회원 로그아웃
+//	@PostMapping("/logout")
+//	public String logout(HttpSession session) {
+//		session.removeAttribute("userDetails");
+//		return "redirect:/thisiswe";
+//	}
+
 	// 회원 로그아웃 Get방식
 	@GetMapping("/logout")
 	public String logoutGet(HttpSession session) {
@@ -80,103 +84,91 @@ public class UserController {
 	// 회원 가입 요청 처리
 	@PostMapping("/signup")
 	public String registerUser(SignupRequestDto requestDto) {
-		
+
 		System.out.println("확인용");
 		userService.registerUser(requestDto);
 		return "/login/login";
 	}
 
 	// 카카오 로그인
-//	@GetMapping("/user/kakao/callback")
 	@GetMapping("/login/oauth2/code/kakao")
 	public String kakaoLogin(@RequestParam String code) throws JsonProcessingException {
 		kakaoUserService.kakaoLogin(code);
-		return "redirect:/";
+		return "redirect:/thisiswe";
 	}
-	
 
 	// 회원정보 조회
 	@GetMapping("/userinfo")
 	public String getUserInfo(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
 		// 첫번째 방법
-		
+
 		model.addAttribute("userId", userDetails.getUsername());
 		model.addAttribute("userNickname", userDetails.getUserEntity().getUserNickname());
 		model.addAttribute("userEmail", userDetails.getUserEntity().getUserEmail());
 
 		return "mypage/userinfo";
 	}
-	
+
 	// 유저아이디 조회
 	@PostMapping("/{userid}")
 	@ResponseBody
 	public ResponseEntity<Boolean> checkUserId(@PathVariable("userid") String userid) {
-		
+
 		System.out.println("$%$#%$#%$#%$#%$#%$#%$#%$%$#%	" + userid);
-		
-		return new ResponseEntity<>(userService.checkUserId(userid) ,HttpStatus.OK);
+
+		return new ResponseEntity<>(userService.checkUserId(userid), HttpStatus.OK);
 	}
-	
+
 	// 유저닉네임 조회
 	@PostMapping("/nickname/{userNickname}")
 	@ResponseBody
 	public ResponseEntity<Boolean> checkUserNickname(@PathVariable("userNickname") String userNickname) {
-		
+
 		System.out.println("$%$#%$#%$#%$#%$#%$#%$#%$%$#%	" + userNickname);
-		
-		return new ResponseEntity<>(userService.checkUserNickname(userNickname) ,HttpStatus.OK);
+
+		return new ResponseEntity<>(userService.checkUserNickname(userNickname), HttpStatus.OK);
 	}
-	
+
 	// 유저 비밀번호 조회 일치 여부확인 후 탈퇴
 	@PostMapping("/user/check/{password}")
 	@ResponseBody
-	public ResponseEntity<String> checkUserPassword(@PathVariable("password") String password, 
+	public ResponseEntity<String> checkUserPassword(@PathVariable("password") String password,
 			@AuthenticationPrincipal UserDetailsImpl userDetails, HttpSession session) {
-		
-        if (userService.checkUserPassword(userDetails.getUsername(), password)) {
-            userService.deleteUser(userDetails.getUsername());
-            session.removeAttribute("userDetails");
-            return new ResponseEntity<>("success", HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("fail", HttpStatus.OK);
-        }
+
+		if (userService.checkUserPassword(userDetails.getUsername(), password)) {
+			userService.deleteUser(userDetails.getUsername());
+			session.removeAttribute("userDetails");
+			return new ResponseEntity<>("success", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("fail", HttpStatus.OK);
+		}
 	}
-	
+
 	// 유저인증코드 보내기
 	@PostMapping("/userEmail/{emailCheck}")
 	@ResponseBody
 	public String pushUserEmailCode(@PathVariable("emailCheck") String userEmail) {
-		
-		if(userService.checkUserEmail(userEmail)) {
+
+		if (userService.checkUserEmail(userEmail)) {
 			return "중복";
-		}
-		else {
+		} else {
 			System.out.println("$%$#%$#%$#%$#%$#%$#%$#%$%$#%	" + userEmail);
 
 			userService.sendVerificationCode(userEmail);
-			
+
 			return "메일 보냄";
 		}
 	}
-	
-//	// 유저인증코드 보내기
-//	@PostMapping("/userEmail/{emailCheck}")
-//	@ResponseBody
-//	public void pushUserEmailCode(@PathVariable("emailCheck") String userEmail) {
-//		
-//		System.out.println("$%$#%$#%$#%$#%$#%$#%$#%$%$#%	" + userEmail);
-//
-//		userService.sendVerificationCode(userEmail);
-//	}
-	
+
 	// 이메일 인증코드 확인
 	@PostMapping("/userEmail/{emailCheck}/{emailCodeCheck}")
 	@ResponseBody
-	public ResponseEntity<Boolean> checkUserEmail(@PathVariable("emailCheck") String userEmail, @PathVariable("emailCodeCheck") String userEmialCode) {
-		
+	public ResponseEntity<Boolean> checkUserEmail(@PathVariable("emailCheck") String userEmail,
+			@PathVariable("emailCodeCheck") String userEmialCode) {
+
 		System.out.println("$%$#%$#%$#%$#%$#%$#%$#%$%$#%	" + userEmail);
 		System.out.println("$%$#%$#%$#%$#%$#%$#%$#%$%$#%	" + userEmialCode);
-		
+
 		return new ResponseEntity<>(userService.verifyCode(userEmail, userEmialCode), HttpStatus.OK);
 	}
 }
